dac8182b3b4a0cc9b802dc2f0080c93f2d386251
NdungutseCharles103 <ndungutsecharles103@gmail.com>
2022-04-01 10:53:33 +0200

    c# calculator

---
+++ C#/Calculator_In_cs/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+   {
+        static void Main(string[] args)
+         {
+            Console.WriteLine("================Simple Calculator=================");
+            for (int i = 0; i < 1; i--)
+            {
+                Console.WriteLine("Enter the first number: ");
+                decimal num1 = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("Enter the second number: ");
+                decimal num2 = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("Enter the operator (+,/,*,-): ");
+                string op = Console.ReadLine();
+
+                switch (op)
+                {
+                    case "+":
+                        Console.WriteLine("The result is " + num1 + num2);
+                        break;
+                    case "-":
+                        Console.WriteLine("The result is " + (num1 - num2));
+                        break;
+                    case "*":
+                        Console.WriteLine("The result is " + num1 * num2);
+                        break;
+                    case "/":
+                        Console.WriteLine("The result is " + num1 / num2);
+                        break;
+                    default:
+                        Console.WriteLine("Please enter the shown operators only");
+                        break;
+                }
+                Console.WriteLine("Do you want to continue?(y/n)");
+                char res =Convert.ToChar(Console.ReadLine());
+                if (res == 'y')
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("===============Thank you for using this program===========");
+                    System.Environment.Exit(0);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+  
+}
---
+++ C#/study/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Type your username and press enter
+      Console.WriteLine("Enter username:");
+
+      // Create a string variable and get user input from the keyboard and store it in the variable
+      string userName = Console.ReadLine();
+
+      Console.WriteLine("Enter your gender: ");
+      string gender = Console.ReadLine();
+      // Print the value of the variable (userName), which will display the input value
+      Console.WriteLine("Your Username is: " + userName + "your gender is: " + gender);
+    }
+  }
+}
