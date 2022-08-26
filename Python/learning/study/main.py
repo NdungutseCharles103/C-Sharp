35a1df257851df02002f063f3d80d23a47a42a83
ndungutsecharles103 <ndungutsecharles103@gmail.com>
2022-08-26 10:18:05 +0200

    added new langs

---
+++ Python/learning/study/main.py
@@ -0,0 +1,28 @@
+# num1 = input("Enter the first number: ")
+# num2 = input("Enter the first number: ")
+#
+# print("sum: "+ str(float(num1)+float(num2)))
+#
+# course = "Coding"
+# print(course.upper())
+# print(course.find("d"))
+# i=0
+# while i < 10:
+#     print(i * '*')
+#     i+=1
+
+numbers =[1, 3,4 ,5,]
+# numbers.append(8)
+# print(numbers)
+# numbers.insert(1, 2)
+# print(numbers)
+# print(len(numbers))
+#
+# for i in numbers:
+#     print("Hello " + str(i))
+#
+num = range(4, 10)
+for i in num:
+    print (i)
+
+print(numbers.count(4))
