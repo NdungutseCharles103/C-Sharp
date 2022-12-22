895961259e9f6f99917a7133f33368918e837ab2
Ndungutse Charles <ndungutsecharles103@gmail.com>
2022-12-22 17:47:42 +0200

    removed unnecessary items

--- C-Sharp/Vidly/Controllers/MovieController.cs
+++ C-Sharp/Vidly/Controllers/MovieController.cs
@@ -12,7 +12,12 @@ namespace Vidly.Controllers
         public IActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            return View(movie);
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
         }
     }
 }
